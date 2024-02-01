747e20bc205dc1b90418e89465868769f0c94766
souravbiswas19 <souravbiswas19april@gmail.com>
2024-02-01 11:40:26 +0530

    commit

--- database.py
+++ database.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "postgresql://postgres:12345@localhost/Quiz"
-engine = create_engine(DATABASE_URL,echo=True,pool_pre_ping=True)
+engine = create_engine("postgresql://postgres:12345@localhost/Quiz",echo=True,pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
